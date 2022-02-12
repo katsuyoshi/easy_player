e4ed316ddb957cfe41ecbd9e4f4be7ad628d9aca
Katsuyoshi Ito <kito@itosoft.com>
2022-02-13 00:46:52 +0900

    noteネームを画面に表示するようにした。volumeも表示。

---
+++ melody.rb
@@ -0,0 +1,66 @@
+# @see https://www.g200kg.com/jp/docs/tech/notefreq.html
+
+def freq key
+  @frequencies[@notes.index(key)]
+end
+
+def note key
+  @notes.index(key)
+end
+
+def define_frequenceies
+  tuning = 440.0
+  keys = %w(C C# D D# E F F# G G# A A# B)
+  @frequencies = []
+  @notes = []
+  128.times do |i|
+    oct = i / 12 - 1
+    key = i % 12
+    freq = (tuning * (2.0 ** ((i - 69.0) / 12.0)) + 0.5).to_i
+    @frequencies << freq
+    @notes << "#{keys[key]}#{oct}"
+  end
+  a = <<EOS
+#ifndef _FREQ_H
+#define _FREQ_H
+EOS
+  a = [a]
+  a <<"uint16_t frequencies[] = {"
+  @frequencies.each_slice(12) do |fs|
+    a << "\t#{fs.map(&:to_s).join(", ")},"
+  end
+  a << "};"
+  a << ""
+  a << "const char *note_names[] = {"
+  @notes.each_slice(12) do |nn|
+    a << "\t#{nn.map{|n| "\"#{n}\""}.join(", ")},"
+  end
+  a << "};"
+  a << ""
+  a << "#endif // _FREQ_H"
+  File.write("src/freq.h", a.join("\n") + "\n")
+end
+
+def define_melody melody
+  oct = "4"
+  keys = melody.scan(/[ABCDEFG]#?\-?\d?/).map{|k| m = /(\-?\d)/ =~ k; oct = $1 if $1; m ? k : "#{k}#{oct}"}
+  a = <<EOS
+#ifndef _MELODY_H
+#define _MELODY_H
+EOS
+  a = [a]
+  a << "int16_t notes[] = {"
+  keys.map{|k| note k}.each_slice(8) do |ns|
+    a << "\t#{ns.map(&:to_s).join(", ")},"
+  end
+  a << "};"
+  a << ""
+  a << "#endif // _MELODY_H"
+  File.write("src/melody.h", a.join("\n") + "\n")
+end
+
+define_frequenceies
+
+zousan = "F4DC FDC FGAC5A4AGFG C5CA4 D5CA4F GADCF"
+
+define_melody zousan
--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,68 @@
 #include "freq.h"
 #include "melody.h"
 
+#define SILENT
+
+#define EMPTY_NOTE   -1
+
 int notes_size = sizeof(notes) / sizeof(notes[0]);
 int notes_index = 0;
+int volume = 1;
+int note = EMPTY_NOTE;
+
+void display() {
+  uint32_t color = GREEN; //GREENYELLOW;
+  // M5.Lcd.clear();
+
+  // 背景表示
+  M5.Lcd.fillRoundRect(0, 0, 320, 240, 4, color);
+
+  if (0 <= note && note <= 127) {
+    M5.Lcd.setCursor(120, 96);
+    M5.Lcd.setTextSize(7);
+    M5.Lcd.setTextColor(BLACK);
+    M5.Lcd.printf(note_names[note]);
+  } else {
+    M5.Lcd.fillRoundRect(110, 120, 100, 8, 2, BLACK);
+    M5.Lcd.fillRect(130, 128, 60, 20,BLACK);
+  }
+
+  for (int i = 1; i <= 10; i++) {
+    if (i <= volume) {
+      M5.Lcd.drawRect(320 - 30, 240 - 30 - (i - 1) * 20, 20, 20, BLACK);
+    } else {
+      M5.Lcd.fillRect(320 - 30, 240 - 30 - (i - 1) * 20, 20, 20, BLACK);
+    }
+  }
+
+}
 
 void setup() {
   M5.begin();
   M5.Lcd.setTextSize(3);
-  M5.Speaker.setVolume(3);
+  M5.Speaker.setVolume(volume);
+  display();
 }
 
 void loop() {
   M5.update();
   if(M5.BtnA.wasPressed()) {
-    int freq = frequencies[notes[notes_index]];
+    note = notes[notes_index];
+    display();
+    int freq = frequencies[note];
     if (++notes_index >= notes_size) notes_index = 0;
+#ifndef SILENT
     M5.Speaker.tone(freq);
+#endif
+    M5.Speaker.setVolume(volume);
+  }
+  if (M5.BtnB.wasPressed()) {
+    volume = (volume + 1) % 11;
+    display();
   }
   if(M5.BtnC.wasPressed()) {
     M5.Speaker.mute();
+    note = EMPTY_NOTE;
+    display();
   }
 }
